39507d2d76c06e6fb2740d2e6c1734794f26dcef
efgfdsdfdf <ezeilodavid292@gmail.com>
2025-10-03 00:52:24 +0100

    Create firebse-messaging-sw.js

---
+++ firebse-messaging-sw.js
@@ -0,0 +1,26 @@
+// firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("Received background message ", payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
